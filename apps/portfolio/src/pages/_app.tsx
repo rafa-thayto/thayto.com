0ae88f95a6b569ad539e90cb3a2399ed2c156b66
Thayto <rafael.thayto@creditas.com>
2022-06-28 22:03:57 -0300

    feat(portfolio): adding favicons

--- apps/portfolio/src/pages/_app.tsx
+++ apps/portfolio/src/pages/_app.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <Component {...pageProps} />
+  <>
+    <Head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+    </Head>
+    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+    <Component {...pageProps} />
+  </>
 )
 
 export default MyApp
