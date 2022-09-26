40728ee096962631da0500957be1589fee9e44a1
Thayto <rafa.thayto@gmail.com>
2022-09-25 22:02:22 -0300

    refactor: adjusting ButtonLink Type

--- packages/ui/ButtonLink.tsx
+++ packages/ui/ButtonLink.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { PropsWithChildren } from 'react'
 
 interface ButtonLinkProps
   extends React.DetailedHTMLProps<
@@ -6,7 +6,10 @@ interface ButtonLinkProps
     HTMLAnchorElement
   > {}
 
-export const ButtonLink: FC<ButtonLinkProps> = ({ children, ...props }) => (
+export const ButtonLink = ({
+  children,
+  ...props
+}: PropsWithChildren<ButtonLinkProps>) => (
   <a
     className="p-2 ml-4 rounded border border-slate-900 hover:bg-yellow-50"
     {...props}
