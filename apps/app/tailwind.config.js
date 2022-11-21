f857b9c6fc57dea140bb44090b2088e7d3bb74c7
Rafael Thayto <rafa.thayto@gmail.com>
2022-11-20 21:51:34 -0300

    style(app): adjusts inline code blocks of @tailwind/typography plugin

--- apps/app/tailwind.config.js
+++ apps/app/tailwind.config.js
@@ -12,7 +12,40 @@ module.exports = {
       serif: ['Lora', ...defaultTheme.fontFamily.serif],
     },
 
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            pre: {
+              color: theme('colors.gray.900'),
+              backgroundColor: theme('colors.slate.200'),
+            },
+            'pre code::before': {
+              'padding-left': 'unset',
+            },
+            'pre code::after': {
+              'padding-right': 'unset',
+            },
+            code: {
+              backgroundColor: theme('colors.slate.200'),
+              color: theme('colors.gray.900'),
+              fontWeight: '400',
+              'border-radius': '0.25rem',
+              'padding-top': '0.1rem',
+              'padding-bottom': '0.1rem',
+            },
+            'code::before': {
+              content: '""',
+              'padding-left': '0.35rem',
+            },
+            'code::after': {
+              content: '""',
+              'padding-right': '0.35rem',
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
