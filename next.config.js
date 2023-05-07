baae7dc645f86acf5409ecf55221d235b3b117be
Rafael Thayto <rafa.thayto@gmail.com>
2023-05-07 07:31:52 -0300

    feat: moves from nx monorepo to next.js app folder

---
+++ next.config.js
@@ -0,0 +1,88 @@
+//@ts-check
+
+// eslint-disable-next-line 
+// @typescript-eslint/no-var-requires
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+const { withPlaiceholder } = require('@plaiceholder/next')
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
+
+  async headers() {
+    // Security Headers based on: https://nextjs.org/docs/advanced-features/security-headers
+    // TODO: implement "Content-Security-Policy" section
+    const securityHeaders = [
+      {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on',
+      },
+      {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload',
+      },
+      {
+        key: 'X-XSS-Protection',
+        value: '1; mode=block',
+      },
+      {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN',
+      },
+      {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=()',
+      },
+      {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+      },
+      {
+        key: 'Referrer-Policy',
+        value: 'origin-when-cross-origin',
+      },
+    ]
+
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+
+      // ENABLES CORS
+      // {
+      //   source: '/api/:path*',
+      //   headers: [
+      //     { key: 'Access-Control-Allow-Credentials', value: 'true' },
+      //     { key: 'Access-Control-Allow-Origin', value: '*' },
+      //     {
+      //       key: 'Access-Control-Allow-Methods',
+      //       value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+      //     },
+      //     {
+      //       key: 'Access-Control-Allow-Headers',
+      //       value:
+      //         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+      //     },
+      //   ],
+      // },
+    ]
+  },
+}
+
+module.exports = withMDX({
+  ...withPlaiceholder(nextConfig),
+  // Append the default value with md extensions
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+})
