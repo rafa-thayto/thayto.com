b331d2042273510ab7c26d99f9ae7a667ec1e7b6
Rafael Thayto <rafa.thayto@gmail.com>
2024-07-07 22:18:22 -0300

    feat(components): adjust layout min height to be the screen

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -32,7 +32,7 @@ export const Layout = ({ children }: PropsWithChildren) => {
   }, [])
 
   return (
-    <div className="bg-gray-100 dark:bg-gray-900">
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
       <Header />
       {children}
       <Footer />
